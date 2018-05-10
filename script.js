37437811eb90ccc2e8aaf8c669128916474ebb40
andriypuklych <avpuklich@gmail.com>
2018-05-10 22:50:43 +0300

    1
    1

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ $(window).resize(function(){
 	$(".tab_item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
 */
-autoSlider();
+/*autoSlider();
 var left = 0;
 var timer;
+*/
 
-
-function autoSlider(){
+/*function autoSlider(){
 	timer = setTimeout(function(){
 		var polosa = document.getElementById('polosa');
 		left = left-128;
@@ -29,3 +29,38 @@ function autoSlider(){
 		autoSlider();
 	}, 1500);
 }
+var slider = document.getElementById('slider');
+// если наведёшь на слайдер, то он остановится
+slider.onmouseover = function() {
+    clearTimeout(timer);
+};
+// если уберешь мышку со слайдера, он вновь запуститься
+slider.onmouseout = function() {
+    autoSlider();
+};﻿*/
+
+var toTop = 0;
+function autoPlay(){
+  timer = setTimeout(function(){
+    var row = document.getElementById('row');
+    toTop = toTop - 128;
+    if (toTop < -512){
+      toTop = 128;
+      clearTimeout(timer);
+      row.onmouseover = clearTimeout(timer);
+      }
+  row.style.top = toTop +'px';
+  autoPlay();
+  }, 1500);
+}
+autoPlay();
+
+var slider = document.getElementById('slider');
+// если наведёшь на слайдер, то он остановится
+slider.onmouseover = function() {
+    clearTimeout(timer);
+};
+// если уберешь мышку со слайдера, он вновь запуститься
+slider.onmouseout = function() {
+    autoSlider();
+};﻿
