58fe68470ba8c52c61276ddd2b6bf518c31fb526
andriypuklych <avpuklich@gmail.com>
2018-05-10 21:42:42 +0300

    1
    1

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+
+function wResize(){
+	$("header").css("height", $(window).height());
+};
+wResize();
+$(window).resize(function(){
+	wResize()
+});
+
+/*$("wrapper .tab").click(function(){
+	$("wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+*/
 autoSlider();
 var left = 0;
 var timer;
@@ -7,7 +21,7 @@ function autoSlider(){
 	timer = setTimeout(function(){
 		var polosa = document.getElementById('polosa');
 		left = left-128;
-		if (left < -640){
+		if (left < -256){
 			left = 128;
 			clearTimeout(timer);
 		}
